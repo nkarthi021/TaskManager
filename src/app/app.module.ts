2a0402282b82d0c24ee2f2550fb30982f24ccf96
nkarthi021 <44129887+nkarthi021@users.noreply.github.com>
2018-10-28 11:06:14 +0530

    added add task, update task and view task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './Task/add-task/add-task.component';
+import { ViewTaskComponent } from './Task/view-task/view-task.component';
+import { UpdateTaskComponent } from './Task/update-task/update-task.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    UpdateTaskComponent
+    
   ],
   imports: [
     BrowserModule,
